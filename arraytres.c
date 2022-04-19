4bcc28482cbd3238219c73ad2e82ce25ba8fe21a
JulyaSouza <julyamsouza27@gmail.com>
2022-04-19 14:07:48 -0300

    aulac

---
+++ arraytres.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<locale.h>
+main(){
+	setlocale(LC_ALL,"Portuguese");
+	int i;
+	float notas[5];
+	printf("Exibir os valores do array \n \n");
+	for (i=0; i<=4; i++){
+		printf("Entre com a nota notas[%d]:",i);
+		scanf("%f",&notas[i]);
+	}
+	for (i=0; i<=4; i=i+1){
+		printf("notas[%d] = %f \n",i, notas[i]);
+	}
+}
